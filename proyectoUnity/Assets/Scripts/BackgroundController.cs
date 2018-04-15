f70c183f3fc2241146ba171da57db396ed492148
Roberto Frías Bernal <roberfrias115@gmail.com>
2018-04-15 21:17:21 +0200

    Project start

---
+++ proyectoUnity/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+	
+	public float scrollVelocity = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<Transform>().position += -new Vector3(0f, scrollVelocity, 0f) * Time.fixedDeltaTime;
+	}
+}
---
+++ proyectoUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float velocity = 1f;
+
+	private float _velocity;
+	private GameObject _background;
+
+	// Use this for initialization
+	void Start () {
+		_velocity = velocity;
+		_background = GameObject.Find("Background");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Movement 
+		//Debug.Log(_background.transform.localScale.x);
+		//if(this.transform.position.x < _background.transform.position.x  &&  this.transform.position.x > -_background.transform.position.x)
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * _velocity;
+	}
+}
